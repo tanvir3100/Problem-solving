be7ded1cf1211b83a7cbadb7835caeeb8f9c7272
tanvir <tanvir0046@gmail.com>
2024-02-23 12:38:46 +0600

    loops and map method's done

---
+++ src/JavaScript/Loops.js
@@ -0,0 +1,58 @@
+/**
+ * -----------------------important Roles in javaScript ---------------------------'-
+ * 1. for loop
+ * const sum = [5, 3, 2, 1, 7];
+ * for(let i = 0; i < sum.length; i++){
+  console.log(sum[i])
+  }
+  Answer: 5 3 2 1 7
+
+
+
+  2. forEach loop
+
+  * const sum = [5, 3, 2, 1, 7];
+  sum.forEach((element)=> {
+  console.log(element * element)
+  })
+
+  Answer: 25 9 4 1 49
+
+  3. Array.from
+  -----This allows you to convert a string collection into an array-----
+  let name = "Harry"
+  let arr = Array.from(name);
+  console.log(arr)
+
+  Answer: 'H', 'a', 'r', 'r', 'y';
+
+
+  4.for......of
+   * const sum = [5, 3, 2, 1, 7];
+
+  -----it is the simplified version of for loop -------
+  ---it will give you the same result as for loop give's you--
+  for(let i of sum){
+  console.log(i)
+  }
+
+    Answer: 5 3 2 1 7
+
+  5.for......in
+  * const sum = [5, 3, 2, 1, 7];
+  -----it will print the index of the array or key of the array-----
+  for(let i in sum){
+  console.log(i)
+  }
+
+  Answer: 0  1  2  3  4
+
+  //but if you need to use it like an for...of loop then you need to added sum[i]
+  then it will give you the same result
+  for(let i in sum){
+  console.log(sum[i])
+  }
+
+  Answer: 5 3 2 1 7
+
+*/
---
+++ src/JavaScript/Map.js
@@ -0,0 +1,14 @@
+// map and forEach
+// --we are using map for make a new array by performing some operation on each array element
+// --we are using forEach for perform some operation on each arry element
+
+//additional argument in forEach and map
+// --we can pass value,index,array to forEach and map
+
+// let arr = [45, 23, 21];
+// let a = arr.map((value, index, array) => {
+//   console.log(value);
+//   return value + 1;
+// });
+
+// console.log(a)
