28bef502c68c5ed5db1126dcc20d7686e2cf6c9c
tanvir <tanvir0046@gmail.com>
2024-02-24 13:14:07 +0600

    write about variable's of javascript

---
+++ src/JavaScript/Variables.js
@@ -0,0 +1,32 @@
+//javaScript have variables
+/**
+variables are containers for storing data (storing data values)
+
+it will help you to store data in a container 
+
+you can store any type of data in a variable 
+
+you can change the value of a variable at any time 
+
+it is a dynamically typed language
+
+javascript allows you to change the type of a variable in run time
+
+
+----SyntexError: 
+javascript is case sensitive language 
+so if your variable name is not same as the variable name in your code then it will show error.
+
+if your write any not allowed keyword which is already reserve in javascript then it will show error.
+
+if you write not allowed keyword in javascript then it will show error.
+*/
+
+
+console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+var a = 67 // a contains 67
+console.log(a)
+a = "harry"
+console.log(a)
+// let 8harry = 7 // Not allowed this will throw an error
+// let var = 7
