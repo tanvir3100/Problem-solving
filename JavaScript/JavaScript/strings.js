016e6b4960b40df9fc4837dfce18a16f7f116248
tanvir <tanvir0046@gmail.com>
2024-02-29 10:41:10 +0600

    write about strings

---
+++ JavaScript/JavaScript/strings.js
@@ -0,0 +1,28 @@
+/*
+String is a primitive data type.
+String is a collection of charachters.
+string is use for manuipulating text.
+
+*/
+
+let name = "Tanvir";
+// console.log(name.length)
+console.log(name[0]);
+console.log(name[1]);
+let friend = "Rofik";
+console.log(friend);
+// let myfriend = "Rofik' // Dont do this
+// console.log(myfriend)
+
+// Template literals
+let boy1 = "sohel";
+let boy2 = "Rana";
+// sohle is a friend of Rana
+let sentence = `${boy2} is a friend of ${boy1}`;
+console.log(sentence);
+
+// Escape Sequence Characters
+// let fruit = 'Bana\'na'
+// console.log(fruit)
+let fruit = 'Bana"na';
+console.log(fruit);
